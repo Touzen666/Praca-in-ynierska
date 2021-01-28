afb54d9b1dea48bb6bf7b12ca5e0faf6b68ce964
Marcin Natanek <marcin.natanek@vodafone.com>
2021-01-28 20:23:43 +0100

    css reset

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
   MatButtonModule,
   MatToolbarModule,
-  MatSliderModule
+  MatSliderModule,
+  MatIconModule,
 ];
 
 @NgModule({

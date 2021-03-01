e8e52b63e7558cb5c8c7d1073b45a8bfa0ed97d8
Bartosz Ochedowski <ochedowski.bartosz@gmail.com>
2021-03-01 18:20:52 +0100

    sum column not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,6 @@ export class AppComponent {
   }
   logout() {
     this.auth.signOut();
+
   }
 }
--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -9,29 +9,44 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort, Sort } from '@angular/material/sort';
 import { TableService } from '../services/table/table.service';
-
+export interface columnSum {
+  quantitySum: number;
+  weightSum: number;
+  caloriesSum: number;
+  carbohydratesSum: number;
+  proteinesSum: number;
+  fatSum: number;
+}
 @Component({
   selector: 'app-tabela',
   templateUrl: './tabela.component.html',
   styleUrls: ['./tabela.component.css']
 })
 export class TabelaComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['name', 'quantity', 'weight', 'energy', 'carbohydrates', 'proteines', 'fat'];
+  displayedColumns: string[] = ['name', 'quantity', 'weight', 'calories', 'carbohydrates', 'proteines', 'fat'];
   public sortedData;
   public products = this.tableService.products;
   public range = this.tableService.range;
   public dataSource = new MatTableDataSource<Produkt>([]);
   public user: firebase.User | null;
-
-
-
+  public productsSum: columnSum;
+  displayedSumColumns: string[] = ['quantity', 'weight', 'calories', 'carbohydrates', 'proteines', 'fat'];
+  columnSum = {
+    quantitySum: 0,
+    weightSum: 0,
+    caloriesSum: 0,
+    carbohydratesSum: 0,
+    proteinesSum: 0,
+    fatSum: 0
+  }
+  sumSource: columnSum;
   // @ViewChild(MatPaginator) paginator: MatPaginator;
   // @ViewChild(MatSort) sort: MatSort;
-  ngAfterViewInit() {
-    // this.dataSource.paginator = this.paginator;
-    // this.dataSource.sort = this.sort;
-  }
+  @ViewChild(MatSort) sort: MatSort;
 
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
   constructor(
     private tableService: TableService,
     private auth: AngularFireAuth
@@ -42,17 +57,30 @@ export class TabelaComponent implements OnInit, AfterViewInit {
     })
 
   }
-
-
-
   ngOnInit(): void {
     this.auth.authState.subscribe((user) => {
       this.user = user;
       this.tableService.refreshSubscription();
     });
+    this.products.subscribe(products => {
+      const produktsList = products;
+      this.sumColumns(produktsList, this.columnSum);
+    })
   }
-
-
+  sumColumns(produktsList, columnSum) {
+    const productsList = produktsList
+    productsList.forEach(p => {
+      columnSum.quantitySum = columnSum.quantitySum + p.quantity
+      columnSum.weightSum = columnSum.weightSum + p.weight
+      columnSum.caloriesSum = columnSum.caloriesSum + p.calories
+      columnSum.carbohydratesSum = columnSum.carbohydratesSum + p.carbohydrates
+      columnSum.proteinesSum = columnSum.proteinesSum + p.proteines
+      columnSum.fatSum = columnSum.fatSum + p.fat
+    })
+    this.sumSource = this.productsSum;
+    this.productsSum = columnSum;
+    return this.productsSum;
+  }
   sortData(sort: Sort) {
     //   const data = this.products.slice();
     //   if (!sort.active || sort.direction === '') {
@@ -75,7 +103,6 @@ export class TabelaComponent implements OnInit, AfterViewInit {
     //     });
     //   }
   }
-
   // function compare(a: number | string, b: number | string, isAsc: boolean) {
   //   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   // }

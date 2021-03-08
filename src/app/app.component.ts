40e4544aca5a4cfbaacdf6de6da2f2d2faef9d6e
Marcin Natanek <marcin.natanek@vodafone.com>
2021-03-08 17:26:06 +0100

    detect facebook messanger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,4 +26,9 @@ export class AppComponent {
     this.auth.signOut();
 
   }
+
+  isFacebookApp() {
+    var ua = navigator.userAgent || navigator.vendor || (window as any).opera;
+    return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
+  }
 }
